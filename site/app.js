ab9ef383496155b6c2ffda82b0b4d65c70d7053a
Haley Siebert <h.k.l.siebert@gmail.com>
2022-10-12 15:03:24 -0700

    Rate limit for real

--- site/app.js
+++ site/app.js
@@ -37,12 +37,16 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req,res){
   res.render("home")
 });
+
 // set up rate limiter: maximum of five requests per minute
 var RateLimit = require('express-rate-limit');
 var limiter = new RateLimit({
   windowMs: 1*60*1000, // 1 minute
   max: 5
 });
+
+app.use(limiter);
+
 app.get("/secret",isLoggedIn ,function(req,res){
     res.render("secret")
 });
